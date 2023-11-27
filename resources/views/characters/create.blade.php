0822debf95155d39dfb1101176fbad0b3c4931ab
Idedav <xdavide.correa@gmail.com>
2023-11-27 17:38:04 +0100

    [add] name on inputs

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -8,51 +8,51 @@
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name">
+            <input type="text" class="form-control" id="name" name="name">
         </div>
         <div class="mb-3">
             <label for="height" class="form-label">Height</label>
-            <input type="number" class="form-control" id="height">
+            <input type="number" class="form-control" id="height" name="height">
         </div>
         <div class="mb-3">
             <label for="weight" class="form-label">Weight</label>
-            <input type="number" class="form-control" id="weight">
+            <input type="number" class="form-control" id="weight" name="weight">
         </div>
         <div class="mb-3">
             <label for="background" class="form-label">Background</label>
-            <input type="text" class="form-control" id="background">
+            <input type="text" class="form-control" id="background" name="background">
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Image Url</label>
-            <input type="text" class="form-control" id="image">
+            <input type="text" class="form-control" id="image" name="image">
         </div>
         <div class="mb-3">
             <label for="armor_class" class="form-label">Armor Class</label>
-            <input type="number" class="form-control" id="armor_class">
+            <input type="number" class="form-control" id="armor_class" name="armor_class">
         </div>
         <div class="mb-3">
             <label for="for" class="form-label">For</label>
-            <input type="number" class="form-control" id="for">
+            <input type="number" class="form-control" id="for" name="for">
         </div>
         <div class="mb-3">
             <label for="des" class="form-label">Des</label>
-            <input type="number" class="form-control" id="des">
+            <input type="number" class="form-control" id="des" name="des">
         </div>
         <div class="mb-3">
             <label for="cos" class="form-label">Cos</label>
-            <input type="number" class="form-control" id="cos">
+            <input type="number" class="form-control" id="cos" name="cos">
         </div>
         <div class="mb-3">
             <label for="int" class="form-label">Int</label>
-            <input type="number" class="form-control" id="int">
+            <input type="number" class="form-control" id="int" name="int">
         </div>
         <div class="mb-3">
             <label for="sag" class="form-label">Sag</label>
-            <input type="number" class="form-control" id="sag">
+            <input type="number" class="form-control" id="sag" name="sag">
         </div>
         <div class="mb-3">
             <label for="car" class="form-label">Car</label>
-            <input type="number" class="form-control" id="car">
+            <input type="number" class="form-control" id="car" name="car">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
